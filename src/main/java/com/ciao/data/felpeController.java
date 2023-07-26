f76174a1dd66241e8e9a9a88764bc11dcb6a8bec
AngeloDima <angelodimauro91@gmail.com>
2023-07-26 12:45:26 +0200

    Delete CRUD

--- src/main/java/com/ciao/data/felpeController.java
+++ src/main/java/com/ciao/data/felpeController.java
@@ -1,10 +1,15 @@
 package com.ciao.data;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.hibernate.internal.util.collections.ConcurrentReferenceHashMap.Option;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +28,26 @@ public class felpeController {
         return felpeRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<felpeModel> detail(@PathVariable("id") Integer id) {
+        Optional<felpeModel> optionalFelpe = felpeRepository.findById(id);
+
+        if (optionalFelpe.isPresent()) {
+            felpeModel felpe = optionalFelpe.get();
+            return ResponseEntity.ok(felpe);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
     @PostMapping("/create")
     public felpeModel createFelpe(@RequestBody felpeModel felpe) {
         return felpeRepository.save(felpe);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") Integer id) {
+        felpeRepository.deleteById(id);
     }
 
 }
